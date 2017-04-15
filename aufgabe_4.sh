003e8118e44eb0cb1d5f7a540695d310357ddf65
Michael Dinitz <michaelsmbp@Michaels-MacBook-Pro.local>
2017-04-15 16:17:23 +0200

    Added testifies and small fix to 4th assignment

--- aufgabe_4.sh
+++ aufgabe_4.sh
@@ -2,6 +2,7 @@
 filename='???'
 for i in `seq 3 6`;
 do
-	find ./ -maxdepth 1 -type f -name "$filename.*"
+	find ./ -maxdepth 1 -type f -name "$filename.*" 
+	find ./ -maxdepth 1 -type f -name "$filename" 
 	filename+='?'
 done
